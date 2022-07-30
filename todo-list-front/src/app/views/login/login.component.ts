9a530ffba6a62d9dd2a1b6aa7da0907c3cb8b648
Rodrigo Prado <rodrigovianaprado@gmail.com>
2022-07-30 08:35:00 +0100

    Add login view

---
+++ todo-list-front/src/app/views/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { LoginService } from 'src/app/core/services/login.service';
+import { LoginDto } from 'src/app/dto/login.dto';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  submitted = false;
+  loginForm: FormGroup;
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private loginService: LoginService
+  ) { }
+
+  ngOnInit() {
+    this.loginForm = this.createForm();
+  }
+
+  createForm(): FormGroup {
+    return this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(form: FormGroup): void {
+    this.submitted = true;
+    if (!form.valid) {
+      return;
+    }
+    const loginDto = Object.assign(new LoginDto(), form.value);
+    this.login(loginDto);
+  }
+
+  login(loginDto: LoginDto): void {
+    this.loginService.login(loginDto)
+    .subscribe(resp => {
+      this.loginService.setCurrentUserSession(resp.user, resp.token);
+      this.router.navigate(['project']);
+    });
+  }
+
+}
